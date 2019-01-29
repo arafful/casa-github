7314cbe95b8749196d34f31b1f86460af380aac2
Andre Rafful <andre.rafful@gmail.com>
2019-01-28 22:05:31 -0200

    Projeto Criado

---
+++ src/curso/Programa.java
@@ -0,0 +1,16 @@
+package curso;
+
+import java.util.Scanner;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Olá mundo");
+
+		sc.close();
+	}
+
+}
